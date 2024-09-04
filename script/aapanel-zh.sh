a0c01ca5edce5cdda312eb4002dbe63c13df2495
Aiko <aikocute@icloud.com>
2024-09-04 22:38:20 +0700

    Update AAPANEL SCRIPT

--- script/aapanel-zh.sh
+++ script/aapanel-zh.sh
@@ -35,7 +35,7 @@ bash "/root/bt-install.sh"
 
 # 降级aapanel GitHub下载 (最后一个无广告版本)
 function downgrade-aapanel(){
-wget -O "/root/LinuxPanel_EN-6.8.23.zip" "https://ghproxy.com/https://github.com/Github-Aiko/aapanel/releases/download/1.0/LinuxPanel_EN-6.8.23.zip"
+wget -O "/root/LinuxPanel_EN-6.8.23.zip" "https://github.com/Github-Aiko/aapanel/releases/download/1.0/LinuxPanel_EN-6.8.23.zip"
 red "下载完成,正在降级."
 unzip LinuxPanel_EN-6.8.23.zip
 cd /root/panel
@@ -47,7 +47,7 @@ rm /root/LinuxPanel_EN-6.8.23.zip /root/panel/ -rf
 
 ## 降级宝塔面板
 function downgrade-bt(){
-wget -O "/root/LinuxPanel-7.7.0.zip" "https://ghproxy.com/https://github.com/Github-Aiko/aaPanel/releases/download/1.3/LinuxPanel-7.7.0.zip"
+wget -O "/root/LinuxPanel-7.7.0.zip" "https://github.com/Github-Aiko/aaPanel/releases/download/1.0/LinuxPanel-7.7.0.zip"
 blue "下载完成,正在降级."
 unzip LinuxPanel-7.7.0.zip
 cd /root/panel
@@ -97,7 +97,7 @@ chattr +i -R /www/server/panel/logs/request
 
 function sinicization-gacjie(){
 # 汉化 来自 https://gitee.com/gacjie/aapanel_chinese 
-wget -O /root/chinese.zip https://ghproxy.com/https://github.com/Github-Aiko/aaPanel/releases/download/1.2/aapanel_chinese-6.8.21.zip
+wget -O /root/chinese.zip https://github.com/Github-Aiko/aaPanel/releases/download/1.0/aapanel_chinese-6.8.21.zip
 unzip -o /root/chinese.zip -d /www/server/
 rm /root/chinese.zip -rf
 /etc/init.d/bt restart
